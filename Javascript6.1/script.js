1ff350a2ea256832bf482ec793b9449d5eccf395
LarsTuns <112869476+LarsTuns@users.noreply.github.com>
2022-10-12 13:27:02 +0200

    Add files via upload

---
+++ Javascript6.1/script.js
@@ -0,0 +1,13 @@
+function changecolor(){
+    alert("het werkt");
+    console.log("Het werkt")
+}
+
+function calculator(num, num2) {
+let answer = num + num2;
+console.log(answer);
+alert(answer);
+
+}
+
+calculator(8, 15)
